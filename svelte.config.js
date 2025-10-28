fdf401975955d0bed762a2f687dd2cb47e8861df
Fardan Hadafi <fardan.hadafi@yahoo.com>
2025-10-29 00:18:37 +0700

    Learn deploy with Github Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,25 @@
-import { asset, assets } from '$app/paths';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: 'index.html',
 			precompress: false,
 			strict: true
 		}),
+		prerender: {
+			entries: ['*']
+		},
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/Learn-Japanese' : ''
+		},
+		files: {
+			lib: 'src/lib'
 		}
 	}
 };
